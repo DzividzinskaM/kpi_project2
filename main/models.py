be45df0109cb84fc064dc80b09c839db62c786a6
OXFORDX <42910683+OXFORDX@users.noreply.github.com>
2019-11-27 16:48:15 +0200

    Added Costume model in models.py, and added this model to admin in admin.py

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Costume(models.Model):
+    costume_name = models.TextField(max_length=30)
+    costume_about = models.TextField(max_length=300)
+    costume_count = models.IntegerField()
+    costume_price = models.IntegerField()
+    costume_image = models.ImageField(upload_to='costume_images/')
